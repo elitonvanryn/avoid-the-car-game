42d686d70a875eafcc9b82709aeb98e514f75553
Eliton Miguel Van Ryn <elitonvanryn@gmail.com>
2024-06-01 12:59:10 -0300

    Created the game window

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
 
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Avoid the car")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+open_window = True
 
+while open_window:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.time.delay(50)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            open_window = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.draw.circle(window, (0, 255, 0), (400, 300), 50)
+    pygame.display.update()
+
+pygame.quit()
